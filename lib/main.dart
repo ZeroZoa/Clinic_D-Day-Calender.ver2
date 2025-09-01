d67881df5cf4e997c89f6601b0e8fcf0fe49b4b7
ZeroZoa <137677773+ZeroZoa@users.noreply.github.com>
2025-09-01 22:41:09 +0900

    확대 축소 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -90,149 +90,152 @@ class _DDayCalculatorPageState extends State<DDayCalculatorPage> {
     final int dDay = _endDate.difference(_startDate).inDays + 1;
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20), // 화면 전체 여백
-        child: Row(
-          children: [
-            const SizedBox(height: 10), // 상단 여백
-            // 시작일 표시 및 선택 버튼
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: InteractiveViewer(
+        boundaryMargin: const EdgeInsets.all(double.infinity),
+          child: Padding(
+            padding: const EdgeInsets.all(20), // 화면 전체 여백
+            child: Row(
               children: [
-                //시작일 파트 시작
-                Padding(
-                    padding: const EdgeInsets.only(left: 15, top: 15),
-                    child: Row(
-                        children: [
-                          Text('시작일', style: TextStyle(fontSize: 24)),
-                          const SizedBox(width: 20),
-                          Text(_formatter.format(_startDate),style: TextStyle(fontSize: 24)),
-                          const SizedBox(width: 20),
-                          ElevatedButton(
-                            onPressed: () => _selectDate(isStart: true),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.indigo, // 배경색
-                              foregroundColor: Colors.white,  // 글자색
-                              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
-                              textStyle: const TextStyle(fontSize: 16),
-                            ),
-                            child: const Text('날짜 선택'),
-                          ),
-                        ]
-                    )
-                ),
-                //시작일 파트 끝
-                const SizedBox(height: 15),
-                //종료일 파트 시작
-                Padding(
-                  padding: const EdgeInsets.only(left: 15),
-                  child: Row(
-                    children: [
-                      Text('종료일', style: TextStyle(fontSize: 24)),
-                      const SizedBox(width: 20),
-                      Text(_formatter.format(_endDate), style: TextStyle(fontSize: 24)),  // 선택된 날짜 표시
-                    ],
-                  ),
-                ),
-                Row(
+                const SizedBox(height: 10), // 상단 여백
+                // 시작일 표시 및 선택 버튼
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      height: 330,
-                      width: 400,
-                      child: CalendarDatePicker(
-                        initialDate: _endDate,
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100),
-                        onDateChanged: (picked) {
-                          setState(() {
-                            _endDate = picked;
-                            if (_endDate.isBefore(_startDate)) {
-                              _errorMessage = '종료일은 시작일 이후여야 합니다.';
-                            } else {
-                              _errorMessage = null;
-                            }
-                          });
-                        },
+                    //시작일 파트 시작
+                    Padding(
+                        padding: const EdgeInsets.only(left: 15, top: 15),
+                        child: Row(
+                            children: [
+                              Text('시작일', style: TextStyle(fontSize: 24)),
+                              const SizedBox(width: 20),
+                              Text(_formatter.format(_startDate),style: TextStyle(fontSize: 24)),
+                              const SizedBox(width: 20),
+                              ElevatedButton(
+                                onPressed: () => _selectDate(isStart: true),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.indigo, // 배경색
+                                  foregroundColor: Colors.white,  // 글자색
+                                  padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
+                                  textStyle: const TextStyle(fontSize: 16),
+                                ),
+                                child: const Text('날짜 선택'),
+                              ),
+                            ]
+                        )
+                    ),
+                    //시작일 파트 끝
+                    const SizedBox(height: 15),
+                    //종료일 파트 시작
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Row(
+                        children: [
+                          Text('종료일', style: TextStyle(fontSize: 24)),
+                          const SizedBox(width: 20),
+                          Text(_formatter.format(_endDate), style: TextStyle(fontSize: 24)),  // 선택된 날짜 표시
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 330,
+                          width: 400,
+                          child: CalendarDatePicker(
+                            initialDate: _endDate,
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2100),
+                            onDateChanged: (picked) {
+                              setState(() {
+                                _endDate = picked;
+                                if (_endDate.isBefore(_startDate)) {
+                                  _errorMessage = '종료일은 시작일 이후여야 합니다.';
+                                } else {
+                                  _errorMessage = null;
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    //종료일 파트 끝
+                    //디데이 파트 시작
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child:Container(
+                        height: 2,
+                        width: 355, // 전체 너비 채움
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, top: 10),
+                      child: _errorMessage != null
+                          ? Text(
+                        _errorMessage!, // 오류 메시지 표시
+                        style: const TextStyle(color: Colors.red),
+                      )
+                          : Text(
+                        '디데이 $dDay일', // 디데이 결과 표시
+                        style: const TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ],
                 ),
-                //종료일 파트 끝
-                //디데이 파트 시작
+                //디데이 파트 끝
+                //단위 별 용량 파트 시작
                 Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                  child:Container(
-                    height: 2,
-                    width: 355, // 전체 너비 채움
-                    color: Colors.grey,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 10),
-                  child: _errorMessage != null
-                      ? Text(
-                    _errorMessage!, // 오류 메시지 표시
-                    style: const TextStyle(color: Colors.red),
-                  )
-                      : Text(
-                    '디데이 $dDay일', // 디데이 결과 표시
-                    style: const TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  padding: const EdgeInsets.only(top: 10, left: 15, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //단위 인디케이터
+                      _buildUnitContainer('2.5단위', dDay * 0.08333333333, [Colors.amber.shade100, Colors.amber.shade50], 2.5, context),
+                      _buildUnitContainer('3단위', dDay * 0.1, [Colors.orange.shade100, Colors.orange.shade50],3, context),
+                      _buildUnitContainer('4단위', dDay * 0.125, [Colors.green.shade100, Colors.green.shade50], 4, context),
+                      _buildUnitContainer('4.3단위', dDay * 0.1428571429, [Colors.teal.shade100, Colors.teal.shade50], 4.3, context),
+                      _buildUnitContainer('5단위', dDay * 0.1666666666, [Colors.blue.shade100, Colors.blue.shade50], 5, context),
+                      _buildUnitContainer('6단위', dDay * 0.2, [Colors.purple.shade100, Colors.purple.shade50], 6, context),
+                      _buildUnitContainer('7.5단위', dDay * 0.25, [Colors.pink.shade100, Colors.pink.shade50], 7.5, context),
+                      const SizedBox(height: 20),
+
+                      //처방 멘트 복사 버튼
+                      Row(
+                        children: [
+                          const SizedBox(width: 181),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              Clipboard.setData(const ClipboardData(text: '호르몬 처방으로 아이동반 내원 D C'));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('아이동반이 복사되었습니다')),
+                              );
+                            },
+                            icon: const Icon(Icons.copy),
+                            label: const Text('아이동반'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.indigo,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                              textStyle: const TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      )
+                      //단위별 용량 파트 및 클립보드 복사 끝
+                    ],
                   ),
                 ),
               ],
             ),
-            //디데이 파트 끝
-            //단위 별 용량 파트 시작
-            Padding(
-              padding: const EdgeInsets.only(top: 10, left: 15, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //단위 인디케이터
-                  _buildUnitContainer('2.5단위', dDay * 0.08333333333, [Colors.amber.shade100, Colors.amber.shade50], 2.5, context),
-                  _buildUnitContainer('3단위', dDay * 0.1, [Colors.orange.shade100, Colors.orange.shade50],3, context),
-                  _buildUnitContainer('4단위', dDay * 0.125, [Colors.green.shade100, Colors.green.shade50], 4, context),
-                  _buildUnitContainer('4.3단위', dDay * 0.1428571429, [Colors.teal.shade100, Colors.teal.shade50], 4.3, context),
-                  _buildUnitContainer('5단위', dDay * 0.1666666666, [Colors.blue.shade100, Colors.blue.shade50], 5, context),
-                  _buildUnitContainer('6단위', dDay * 0.2, [Colors.purple.shade100, Colors.purple.shade50], 6, context),
-                  _buildUnitContainer('7.5단위', dDay * 0.25, [Colors.pink.shade100, Colors.pink.shade50], 7.5, context),
-                  const SizedBox(height: 20),
-
-                  //처방 멘트 복사 버튼
-                  Row(
-                    children: [
-                      const SizedBox(width: 181),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Clipboard.setData(const ClipboardData(text: '호르몬 처방으로 아이동반 내원 D C'));
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('아이동반이 복사되었습니다')),
-                          );
-                        },
-                        icon: const Icon(Icons.copy),
-                        label: const Text('아이동반'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.indigo,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                          textStyle: const TextStyle(fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  )
-                  //단위별 용량 파트 및 클립보드 복사 끝
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+      )
     );
   }
 }
